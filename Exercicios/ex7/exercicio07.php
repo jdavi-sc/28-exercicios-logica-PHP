f83f6a20e39e14ea90da98925b9803b658b75583
José Davi <josedscosta2005@gmail.com>
2025-05-16 13:55:22 -0300

    Realizado o exercicio 07

--- Exercicios/ex7/exercicio07.php
+++ Exercicios/ex7/exercicio07.php
@@ -1,9 +1,26 @@
 <?php
 
-     $primeiroNumero = $_POST["primeiroNumero"];
+    $primeiroNumero = $_POST["primeiroNumero"];
     $segundoNumero = $_POST["segundoNumero"];
     $terceiroNumero = $_POST["terceiroNumero"];
 
-    
+    if ($primeiroNumero > $segundoNumero && $primeiroNumero > $terceiroNumero && $segundoNumero < $terceiroNumero) {
+        echo "O maior número é o $primeiroNumero e o menor é o $segundoNumero";
+    } else if ($primeiroNumero > $segundoNumero && $primeiroNumero > $terceiroNumero && $terceiroNumero < $segundoNumero) {
+        echo "O maior número é o $primeiroNumero e o menor é o $terceiroNumero";
+
+    } else if ($segundoNumero > $primeiroNumero && $segundoNumero > $terceiroNumero && $primeiroNumero < $terceiroNumero) {
+        echo "O maior número é o $segundoNumero e o menor é o $primeiroNumero";
+    } else if ($segundoNumero > $primeiroNumero && $segundoNumero > $terceiroNumero && $terceiroNumero < $primeiroNumero) {
+        echo "O maior número é o $segundoNumero e o menor é o $terceiroNumero";
+
+    } else if ($terceiroNumero > $primeiroNumero && $terceiroNumero > $segundoNumero && $primeiroNumero < $segundoNumero) {
+        echo "O maior número é o $terceiroNumero e o menor é o $primeiroNumero";
+    } else if ($terceiroNumero > $primeiroNumero && $terceiroNumero > $segundoNumero && $segundoNumero < $primeiroNumero) {
+        echo "O maior número é o $terceiroNumero e o menor é o $segundoNumero";
+
+    } else {
+        echo "Informe números com os valores diferentes";
+    }
 
 ?>
